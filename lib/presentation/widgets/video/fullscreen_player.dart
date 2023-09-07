e31558bca4a33fdd9cfc7beca1c84d5f7ef5bf1f
Alejandro <alekungfumdp@gmail.com>
2023-09-07 17:21:54 -0300

    finish app

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toktik/presentation/widgets/video/video_background.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenPlayer extends StatefulWidget {
@@ -23,10 +24,7 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
     controller = VideoPlayerController.asset(widget.videoUrl)
       ..setVolume(0)
       ..setLooping(true)
-      ..play()
-      ..initialize().then((_) {
-        setState(() {});
-      });
+      ..play();
   }
 
   @override
@@ -57,6 +55,9 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
             child: Stack(
               children: [
                 VideoPlayer(controller),
+                VideoBackground(
+                  stops: const [0.8, 1.0],
+                ),
                 Positioned(
                   bottom: 50,
                   left: 20,
