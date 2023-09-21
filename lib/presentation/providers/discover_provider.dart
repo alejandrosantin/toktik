3c3fbf9473b0fcf5a45fe41d5cf04a5caa1fa964
Alejandro <alekungfumdp@gmail.com>
2023-09-21 10:41:24 -0300

    repositories and datasources

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/domain/repositories/video_post_repository.dart';
-import 'package:toktik/shared/data/local_video_posts.dart';
-
-import '../../infrastructure/models/local_video_model.dart';
 
 class DiscoverProvider extends ChangeNotifier {
   final VideoPostRepository videoPostRepository;
